042b06fd745f7ec49a26310a198b562bfdbb8efd
tarhi mouad <mouad.tarhi@gmail.com>
2024-05-16 19:21:06 +0200

    update config

--- app/main.py
+++ app/main.py
@@ -12,8 +12,4 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    a=1+1
-    print(a)
-    a=a+1
-    print(a)
     return {"item_id": item_id, "q": q}
